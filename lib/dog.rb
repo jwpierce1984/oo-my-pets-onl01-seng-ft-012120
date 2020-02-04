f0cb86642e13dfb59d4ba623bab018059acb20c5
Justin Pierce <jwpierce1984@gmail.com>
2020-02-04 21:51:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
   
   def initialize (name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
   
 end
